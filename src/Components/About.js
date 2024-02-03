1b838b7d5b69d5e1eeffd8eb0c08f7b5df312929
anu0108 <108670241+anu0108@users.noreply.github.com>
2024-02-03 19:24:46 +0530

    fix: about

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,44 +2,58 @@ import "../Styles/about.css";
 import Lottie from "lottie-react";
 import Developer_Yogi from "../LottieFiles/Developer_Yogi.json";
 import Skills from "./Skills";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <>
-    <div className="about-container">
-      <Lottie
-        className="developer-yogi"
-        animationData={Developer_Yogi}
-        loop={true}
-      />
-      <div className="about-text">
-        <h2>Learn About Me</h2>
-        <p id="para1">
-          I'm a final year college student pursuing <b>B.Tech in CSE</b> from
-          New Delhi, India. With a strong love for web development, I specialize
-          in the <b>MERN stack</b>, showcasing a keen understanding of data
-          structures, algorithms, and robust problem-solving skills.
-        </p>
+      <div className="about-container">
+        <Lottie
+          className="developer-yogi"
+          animationData={Developer_Yogi}
+          loop={true}
+        />
+        <div className="about-text">
+          <h2>Learn About Me</h2>
+          <p id="para1">
+            I'm a final year college student pursuing <b>B.Tech in CSE</b> from
+            New Delhi, India. With a strong love for web development, I
+            specialize in the <b>MERN stack</b>, showcasing a keen understanding
+            of data structures, algorithms, and robust problem-solving skills.
+          </p>
 
-        <p>
-          My tech journey pivoted during my <b>internship</b> as a front-end
-          developer at Bharat Heavy Electricals Limited, Delhi. It was here that
-          I refined my skills, gained real-world experience and enhanced my
-          ability to create flawless user interfaces.
-        </p>
+          <p>
+            My tech journey pivoted during my <b>internship</b> as a front-end
+            developer at Bharat Heavy Electricals Limited, Delhi. It was here
+            that I refined my skills, gained real-world experience and enhanced
+            my ability to create flawless user interfaces.
+          </p>
 
-        <p>
-          Open to challenges, I'm eager for <b>collaborations</b>, constantly
-          learning, committed to meaningful projects. <b>Join</b> me on this
-          exciting coding expedition, where innovation meets dedication, and
-          every line of code paves the way for digital excellence. Explore some
-          of my work in the <b>Projects</b> section. 
-        </p>
+          <p>
+            I also undertook an internship at
+            <Link href="https://www.linkedin.com/company/fitverse-fit/">
+              <b> Fitverse</b>
+            </Link>
+            , where I played a key role in developing their UI and their
+            software Gravity. This experience further diversified my skill set
+            and exposed me to different facets of the industry.
+          </p>
 
-        <p>Beyond the field of technology, I find joy in singing and playing the guitar.</p>
+          <p>
+            Open to challenges, I'm eager for <b>collaborations</b>, constantly
+            learning, committed to meaningful projects. <b>Join</b> me on this
+            exciting coding expedition, where innovation meets dedication, and
+            every line of code paves the way for digital excellence. Explore
+            some of my work in the <b>Projects</b> section.
+          </p>
+
+          <p>
+            Beyond the field of technology, I find joy in singing and playing
+            the guitar.
+          </p>
+        </div>
       </div>
-    </div>
-    <Skills/>
+      <Skills />
     </>
   );
 };
