f3247af732e375e2d297f814d1d27621365c7b02
anuragtj <anuragwadhwa786@gmail.com>
2024-08-12 18:58:52 +0530

    fix: navbar link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -60,26 +60,24 @@ const Header = () => {
 
         {isClicked && (
           <div className="dropdown-menu">
-            <Link to="/" className="nav-links" id="home">
-              {" "}
-              HOME{" "}
-            </Link>
-            <Link to="/about" className="nav-links">
-              {" "}
-              ABOUT{" "}
-            </Link>
-            <Link to="/projects" className="nav-links">
-              {" "}
-              PROJECTS{" "}
-            </Link>
-            <Link to="/resume" className="nav-links">
-              {" "}
-              RESUME{" "}
-            </Link>
-            <Link to="/contact" className="nav-links">
-              {" "}
-              CONTACT{" "}
-            </Link>
+           <HashLink smooth to="/#" className="nav-links">
+            HOME
+          </HashLink>
+          <HashLink smooth to="/#about" className="nav-links">
+            ABOUT
+          </HashLink>
+          <HashLink smooth to="/#experience" className="nav-links">
+            EXPERIENCE
+          </HashLink>
+          <HashLink smooth to="/#projects" className="nav-links">
+            PROJECTS
+          </HashLink>
+          <Link to="/resume" className="nav-links">
+            RESUME
+          </Link>
+          <HashLink smooth to="/#contact" className="nav-links">
+            CONTACT
+          </HashLink>
           </div>
         )}
       </div>
